07880e1c647e05c3400a17485ff37fb6de23fc19
Mohamed Ebrahim Atia <eng.mohamed@hotmail.ca>
2022-11-08 16:16:49 +0200

    cleaned

--- TestWebApi/Controllers/ValuesController.cs
+++ TestWebApi/Controllers/ValuesController.cs
@@ -4,36 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using UMIS_VER2.AccessLayer.Bases;
+using UMIS_VER2.AccessLayer;
+using UMIS_VER2.BusinessLyer;
 
 namespace TestWebApi.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public object Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            TList<AsCodeDegreeClass> lstAsCodeDegreeClass = new TList<AsCodeDegreeClass>();
+            AsCodeDegreeClassParameterBuilder objAsCodeDegreeClassParameterBuilder = new AsCodeDegreeClassParameterBuilder();
+            lstAsCodeDegreeClass = DataRepository.AsCodeDegreeClassProvider.Find(objAsCodeDegreeClassParameterBuilder.GetParameters());
+            return lstAsCodeDegreeClass.Select(o => new { o.AsCodeDegreeClassId, o.ClassDescrEn, o.ClassDescrAr }).ToList();
         }
     }
 }
